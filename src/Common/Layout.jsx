017d50fc27ebc2889631194edbbd162685cf2839
kevin <kevinshah12jan@gmail.com>
2026-09-19 11:34:34 +0530

    scroll to top added

--- src/Common/Layout.jsx
+++ src/Common/Layout.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Navbar from '../Component/Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from '../Component/Footer'
+import ScrollToTop from '../Component/ScrollToTop'
 
 const Layout = () => {
   return (
     <>
+    <ScrollToTop />
     <Navbar />
     <Outlet />
     <Footer />
